a4aec03783752ea715016e7fe217f2c194350201
Shambhavi <shambhavi943@gmail.com>
2020-08-30 11:51:48 +0530

    Updates

---
+++ precache-manifest.73b83e3fa44fb958fed15860026c9917.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2ffb6729eb03e500a3a3b124ce4fa2",
+    "url": "/list/index.html"
+  },
+  {
+    "revision": "dce2bd73539e92d84dcc",
+    "url": "/list/static/css/main.34e27853.chunk.css"
+  },
+  {
+    "revision": "d09401dd82f4d495427a",
+    "url": "/list/static/js/2.ad5aac3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/list/static/js/2.ad5aac3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce2bd73539e92d84dcc",
+    "url": "/list/static/js/main.2fb4e31e.chunk.js"
+  },
+  {
+    "revision": "89d14599cc7cafba567f",
+    "url": "/list/static/js/runtime-main.d8e0fdbd.js"
+  },
+  {
+    "revision": "180a9794a6487acdc7902a1cb9abe0e8",
+    "url": "/list/static/media/1.180a9794.jpg"
+  }
+]);
